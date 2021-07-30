749713b83d6937d8b1fb5396f5bb0af4c6a4546d
Sandesh <sandesh2643@gmail.com>
2021-07-30 21:28:11 +0530

    Updated action

--- src/action.js
+++ src/action.js
@@ -1,5 +1,10 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run() {
+	const actionType = core.getInput('type');
 	console.log('Hello, world!');
+	console.log(`Action type is : ${actionType}!`);
 }
 
 run();
