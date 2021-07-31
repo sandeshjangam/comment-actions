031205a1300406d2c32838c0174070d7f835b8b6
Sandesh <sandesh2643@gmail.com>
2021-07-31 13:26:55 +0530

    debug

--- src/action.js
+++ src/action.js
@@ -4,14 +4,39 @@ const { inspect } = require("util");
 
 async function run() {
 
+	let actionType, body, issueNumber, owner, repo;
+
+	// function create_comment() {
+		
+	// 	if (inputs.issueNumber) {
+	// 		// Create a comment
+	// 		if (!inputs.body) {
+	// 			core.setFailed("Missing comment 'body'.");
+	// 			return;
+	// 		}
+	// 		const { data: comment } = await octokit.rest.issues.createComment({
+	// 			owner: repo[0],
+	// 			repo: repo[1],
+	// 			issue_number: inputs.issueNumber,
+	// 			body: inputs.body,
+	// 		});
+	// 		core.info(
+	// 			`Created comment id '${comment.id}' on issue '${inputs.issueNumber}'.`
+	// 		);
+	// 		core.setOutput("comment-id", comment.id);
+	// 	}
+	// }
+
 	try {
 		
 		const repository = core.getInput('repository');
-		const actionType = core.getInput('type');
-		const body = core.getInput('body');
-		const issueNumber = core.getInput('number');
 		
-		const [owner, repo] = repository ? repository.split('/') : process.env.GITHUB_REPOSITORY.split('/');
+		// Assign variables.
+		actionType = core.getInput('type');
+		body = core.getInput('body');
+		issueNumber = core.getInput('number');
+		[owner, repo] = repository ? repository.split('/') : process.env.GITHUB_REPOSITORY.split('/');
+		
 
 		console.log('Hello, world!');
 		console.log(`Environment : ${inspect(process.env)}`);
